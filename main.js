1151325027d855177d513773d527ad770225b729
Felipe Mattiassi <fepazmattiassi@gmail.com>
2023-03-29 19:05:18 -0300

    torll hyper

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-while (true) {
-    console.log('hahahahahahahahhahahahahhahahahahahahahhahahahahahahahahahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+let au = document.querySelector('audio')
+au.setAttribute('src','./musica_module 3.mp3')
+let header = document.querySelector('header')
+let img = document.querySelector('img')
+function clicou() {
+    au.play()
+    window.alert('AGORA SEU PC É MEU!!!!!!!!!!!')
+    document.body.style.backgroundColor = 'black'
+    header.style.backgroundColor = 'black'
+    img.setAttribute('src', './imagens/fotocaveira.jpeg')
+    while (true) {
+        console.log('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhdddddddddddgdgdgdgdgdgdgddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddgggggggggggggggggggggggahahahahahahahhahahahahhahahahahahahahhahahahahahahahahahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+    }
 }
